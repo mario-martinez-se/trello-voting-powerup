f6a1fa13960356d24d223b0ef5c2b33a7ab21890
Glitch (rain-diamond) <none>
2018-02-09 11:32:28 +0000

    🎰🚦 Checkpoint public/vote.html:461372/9 public/js/vote.js:461372/218

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,9 +1,13 @@
 var t = TrelloPowerUp.iframe();
 
-window.estimate.addEventListener('submit', function(event){
+window.vote.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  return t.set('card', 'shared', 'vote', window.voteNumber.value)
+  //return t.set('card', 'shared', 'vote', window.voteNumber.value)
+  return t.get('member', 'shared', 'remaining', 3)
+  .then(function(remaining){
+    
+  })
   .then(function(){
     t.closePopup();
   });
