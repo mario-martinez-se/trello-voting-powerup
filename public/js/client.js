1733131eab34e7fd91690e564975cc216c5ccb12
Glitch (rain-diamond) <none>
2018-02-09 09:28:59 +0000

    🕍😀 Checkpoint public/js/client.js:461372/111 public/manifest.json:461372/18

--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,8 @@
 
 var Promise = TrelloPowerUp.Promise;
 
+var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
+var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 TrelloPowerUp.initialize({
@@ -10,12 +12,22 @@ TrelloPowerUp.initialize({
 		return [{
 			icon: BLACK_ROCKET_ICON,
 			text: 'Estimate Size',
-	callback: function(t) {
-	return t.popup({
-	title: "Estimation",
-	url: 'estimate.html',
-	});
+      callback: function(t) {
+      return t.popup({
+        title: "Estimation",
+        url: 'estimate.html',
+      });
 	}
 		}];
 	},
+  'card-badges': function(t, options) {
+    return t.get('card', 'shared', 'estimate')
+    .then(function(estimate) {
+      return [{
+        icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
+        text: estimate || 'No Estimate!',
+        color: estimate ? null : 'red',
+      }Z];  
+    });
+  }
 });
