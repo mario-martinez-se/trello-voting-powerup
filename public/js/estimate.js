860a1aebf5b9ea4d2c55759be2c9ebab283f74d4
Glitch (rain-diamond) <none>
2018-02-09 09:17:59 +0000

    🌭🌭 Checkpoint public/js/estimate.js:461372/9

--- public/js/estimate.js
+++ public/js/estimate.js
@@ -1,5 +1,20 @@
 var t = TrelloPowerUp.iframe();
 
+window.estimate.addEventListener('submit', function(event){
+  // Stop the browser trying to submit the form itself.
+  event.preventDefault();
+  return t.set('card', 'shared', 'estimate', window.estimateSize.value)
+  .then(function(){
+    t.closePopup();
+  });
+});
+
 t.render(function(){
-  t.sizeTo('#estimate').done();
+  return t.get('card', 'shared', 'estimate')
+  .then(function(estimate){
+    window.estimateSize.value = estimate;
+  })
+  .then(function(){
+    t.sizeTo('#estimate').done();
+  });
 });
