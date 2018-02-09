1a63feecb660e9750ca87dcf411f30dd1f4b153d
Glitch (rain-diamond) <none>
2018-02-09 12:03:24 +0000

    ⛱🌻 Checkpoint public/vote.html:461372/1 public/js/vote.js:461372/862

--- public/js/vote.js
+++ public/js/vote.js
@@ -3,26 +3,35 @@ var t = TrelloPowerUp.iframe();
 window.vote.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
+  var selectedVote = $('#voteNumber').val();
+  return t.get('member', 'shared', 'remaining', 3)
+  .then(function(remaining) {
+    return t.set('member', 'shared', 'remaining', remaining - selectedVote);
+  })
+  .then(function() {
+    return t.get('card', 'shared', 'vote', 0);
+  })
+  .then(function(currentCount) {
+    return t.set('card', 'shared', 'vote', currentCount + selectedVote)
+  })
   //return t.set('card', 'shared', 'vote', window.voteNumber.value)
   t.closePopup();
 });
 
 t.render(function(){
   return t.get('member', 'shared', 'remaining', 3)
-  .then(function(remaining){
+  .then(function(remaining) {
     var options = [];
     for (var i = 0; i < remaining; i++) {
       options.push(i+1);
     }
-    
     return options;
   })
-  .then(function(){
-    $(document).ready(function () {
-      $('#vote').hide();
-    });
-    
-    //$('#voteNumber option[value="1"]').attr('disabled','disabled');
+  .then(function(availableOptions){
+    $('#voteNumber option').attr('disabled','disabled');
+    for (var i=0; i<availableOptions.length; i++) {
+      $('#voteNumber option[value="'+ availableOptions[i] +'"]').removeAttr('disabled');
+    }
     t.sizeTo('#vote').done();
   });
 });
