2eacc80bdfda90505f21fa37e4dc86912ee01527
Glitch (rain-diamond) <none>
2018-02-09 11:42:45 +0000

    🐁🌆 Checkpoint public/js/vote.js:461372/497 public/vote.html:461372/12

--- public/js/vote.js
+++ public/js/vote.js
@@ -4,19 +4,21 @@ window.vote.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
   //return t.set('card', 'shared', 'vote', window.voteNumber.value)
-  return t.get('member', 'shared', 'remaining', 3)
-  .then(function(remaining){
-    
-  })
-  .then(function(){
-    t.closePopup();
-  });
+  t.closePopup();
 });
 
 t.render(function(){
-  return t.get('card', 'shared', 'vote')
-  .then(function(vote){
-    window.voteNumber.value = vote;
+  return t.get('member', 'shared', 'remaining', 3)
+  .then(function(remaining){
+    var options = [];
+    for (var i = 0; i < remaining; i++) {
+      options.push(i+1);
+    }
+    
+    return options;
+  })
+  .then(function(){
+    $('')
   })
   .then(function(){
     t.sizeTo('#vote').done();
